67b6217a34790da31f25c02a345ea7ab7528ad7a
Ricardo Ancona <ricardo.ancona22@gmail.com>
2021-01-05 13:13:33 -0600

    Update colors

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          dark: '#000925',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
